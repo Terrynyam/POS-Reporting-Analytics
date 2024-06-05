ee14562539c09a064453a2847f95f3802ae5e5a1
terrancen <terrancen@afrosoft.co.zw>
2024-06-05 18:24:19 +0200

    initial commit

---
+++ src/main/java/com/tenic/reporting_analytics/ReportingAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.tenic.reporting_analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportingAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportingAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenic/reporting_analytics/ReportingAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenic.reporting_analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
